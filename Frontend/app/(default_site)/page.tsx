0a1e5cfc41718dc799d11ffe2eea59c9c0ed0a44
Manas <iamscientistmanas@gmail.com>
2025-07-30 20:21:57 +0530

    Error captured - add somne components back

--- Frontend/app/(default_site)/page.tsx
+++ Frontend/app/(default_site)/page.tsx
@@ -1,12 +1,12 @@
 import { Hero } from "@/app/ui/landing/Hero";
 import { Skills } from "@/app/ui/landing/Skills";
-// import { Projects } from "@/app/ui/landing/Projects";
+import { Projects } from "@/app/ui/landing/Projects";
 import { Flex } from "@/app/ui/landing/Flex";
 import { CommunityWork } from "@/app/ui/landing/CommunityWork";
 import { MoreAboutMe } from "@/app/ui/landing/MoreAboutMe";
 // import { Contact } from "@/app/ui/landing/Contact";
-// import { GithubMap } from "@/app/ui/components/github-map";
-// import { UserInfo } from "@/app/ui/landing/UserInfo";
+import { GithubMap } from "@/app/ui/components/github-map";
+import { UserInfo } from "@/app/ui/landing/UserInfo";
 
 export default function Home() {
   return (
@@ -15,16 +15,16 @@ export default function Home() {
         <div className="wrapper relative w-full max-w-screen-xl h-fit flex flex-col gap-14 lg:gap-28 overflow-hidden py-4 px-5 items-center justify-center">
           <Hero />
           <Skills />
-          {/* <Projects /> */}
+          <Projects />
           <Flex />
           <CommunityWork />
           <MoreAboutMe />
           {/* <Contact /> */}
-          {/* <GithubMap /> */}
+          <GithubMap />
         </div>
         {/* For less space at bottom */}
         <div className="wrapper relative w-full max-w-screen-xl h-fit flex overflow-hidden px-5 items-center justify-center">
-          {/* <UserInfo /> */}
+          <UserInfo />
         </div>
       </div>
     </div>
